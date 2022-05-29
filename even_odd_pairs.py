134cd73a823e6bff937e54a6e9edba956ca370d4
Manoj <97424621+manojmanu34@users.noreply.github.com>
2022-05-29 19:05:36 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] c=[] if(n%2==0):     for i in range(0,n):         if(a[i]%2==0):             b.append(a[i])         else:             c.append(a[i])     for i in range(len(a)):         if i<len(b):             print(b[i],end=" ")         if i<len(c):             print(c[i],end=" ")
    else:
        for i in range(0,n):
            if(a[i]%2==0):
                b.append(a[i])
            else:
                c.append(a[i])
        for i in range(len(a)):
            if i<len(b):
                print(b[i],end=" ")
            if i<len(c):
                print(c[i],end=" ")
        print(0)

---
+++ even_odd_pairs.py
@@ -0,0 +1,31 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=[]
+if(n%2==0):
+    for i in range(0,n):
+        if(a[i]%2==0):
+            b.append(a[i])
+        else:
+            c.append(a[i])
+    for i in range(len(a)):
+        if i<len(b):
+            print(b[i],end=" ")
+        if i<len(c):
+            print(c[i],end=" ")
+        
+else:
+    for i in range(0,n):
+        if(a[i]%2==0):
+            b.append(a[i])
+        else:
+            c.append(a[i])
+    for i in range(len(a)):
+        if i<len(b):
+            print(b[i],end=" ")
+        if i<len(c):
+            print(c[i],end=" ")
+    print(0) 
+    
+        
+    
